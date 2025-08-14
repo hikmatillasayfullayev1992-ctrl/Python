cd6a213467796770c70614b22afb224ae02f3e37
hikmatillasayfullayev1992-ctrl <hikmatillasayfullayev1992@gmail.com>
2025-08-14 18:00:42 +0500

    Update 707.py

--- lesson-1/homework/707.py
+++ lesson-1/homework/707.py
@@ -1,1 +1,19 @@
-
+# 1 mashina nomi
+n = "MsaatmiazD"
+print(n[::-2])
+print(n[::2])
+    # 2  aylana uzunligi
+na=int(input("diametrni kiriting"))
+print(na*3.14)
+    # 3 kvadrat perimetri va yuzi
+side=5
+print("perimetr", side*4)
+print("yuzi",side**2)
+     # 3 ikki son o`rtachasi
+aa=15
+bb=25
+print((aa+bb)/2)
+    # yig`indi ko`paytma, kvadrat
+print(aa+bb)
+print(aa*bb)
+print(aa**bb)
