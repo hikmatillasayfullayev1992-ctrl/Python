e581520eaffcd506efc898c6607f154a75246acf
hikmatillasayfullayev1992-ctrl <hikmatillasayfullayev1992@gmail.com>
2025-08-14 18:04:25 +0500

    Update 707.py

--- lesson-2/homework/707.py
+++ lesson-2/homework/707.py
@@ -1,1 +1,8 @@
-
+## 1 age kalkulyator
+a=input("Ismingizni kiriting: ")
+b=input("Tug`ilgan yilizni kiriting: ")
+print(f"{a} sizning yoshingiz {2025-int(b)}da")
+  ## 2. Extract Car Names
+txt = 'LMaasleitbtui'
+print(txt[::2])
+print(txt[1::2])
